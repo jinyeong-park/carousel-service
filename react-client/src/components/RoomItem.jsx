548029d253c83494c99eb381f54b7bd5a1513312
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-18 23:40:01 -0700

    completed sead data.

--- react-client/src/components/RoomItem.jsx
+++ react-client/src/components/RoomItem.jsx
@@ -9,7 +9,7 @@ function RoomItem(props) {
     // console.log('props after return', props)
     <div>
 
-      <img src={props.room.room_photo.imageUrl} onClick={props.handleClick} className="feed-list-item-image"/>
+      <img src={props.room.imageUrl} onClick={props.handleClick} className="feed-list-item-image"/>
       <span className="feed-list-item-type">{props.room.type}</span>
       <span className="feed-list-item-bed"> · {props.room.bed > 1 ? props.room.bed +' beds' : props.room.bed + ' bed'} </span>
       <div className="feed-list-item-title">{props.room.title}</div>
